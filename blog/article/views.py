68e166a1fe87584210ac135b6086a30f62f66351
Vitaly <info@myflyup.ru>
2023-03-16 16:29:09 +0300

    hw for lesson 7 completed

--- blog/article/views.py
+++ blog/article/views.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import login_required, current_user
 from werkzeug.exceptions import NotFound
+from sqlalchemy.orm import joinedload
 
 from blog.extensions import db
 from blog.forms.article import CreateArticleForm
-from blog.models import Article, Author
+from blog.models import Article, Author, Tag
 
 article = Blueprint('article', __name__, url_prefix='/articles', static_folder='../static')
 
@@ -20,7 +21,12 @@ def article_list():
 
 @article.route('/<int:article_id>/', methods=['GET'])
 def article_detail(article_id):
-    _article: Article = Article.query.filter_by(id=article_id).one_or_none()
+    _article: Article = Article.query.filter_by(
+        id=article_id
+    ).options(
+        joinedload(Article.tags)
+    ).one_or_none()
+
     if _article is None:
         raise NotFound
     return render_template(
@@ -29,10 +35,23 @@ def article_detail(article_id):
     )
 
 
+# Вьюха до 7 урока:
+# @article.route('/<int:article_id>/', methods=['GET'])
+# def article_detail(article_id):
+#     _article: Article = Article.query.filter_by(id=article_id).one_or_none()
+#     if _article is None:
+#         raise NotFound
+#     return render_template(
+#         'articles/details.html',
+#         article=_article,
+#     )
+
+
 @article.route('/create/', methods=['GET'])
 @login_required
 def create_article_form():
     form = CreateArticleForm(request.form)
+    form.tags.choices = [(tag.id, tag.name) for tag in Tag.query.order_by('name')]
     return render_template('articles/create.html', form=form)
 
 
@@ -40,8 +59,12 @@ def create_article_form():
 @login_required
 def create_article():
     form = CreateArticleForm(request.form)
+
+    form.tags.choices = [(tag.id, tag.name) for tag in Tag.query.order_by('name')]
+
     if form.validate_on_submit():
         _article = Article(title=form.title.data.strip(), text=form.text.data)
+
         if current_user.author:
             _article.author_id = current_user.author.id
         else:
@@ -49,6 +72,11 @@ def create_article():
             db.session.add(author)
             db.session.flush()
             _article.author_id = author.id
+
+        if form.tags.data:
+            selected_tags = Tag.query.filter(Tag.id.in_(form.tags.data))
+            for tag in selected_tags:
+                _article.tags.append(tag)
 
         db.session.add(_article)
         db.session.commit()
